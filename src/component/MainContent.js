4f4761dda4b7cad562630d27fdf10d939e9e4ea9
aniloli42 <aniloli42@gmail.com>
2021-08-18 19:46:52 +0545

    React Blog App First Commit

---
+++ src/component/MainContent.js
@@ -0,0 +1,10 @@
+import React from "react";
+// import PropTypes from "prop-types";
+
+export default function MainContent(props) {
+  return (
+    <main>
+      <p>This is Main Content</p>
+    </main>
+  );
+}
